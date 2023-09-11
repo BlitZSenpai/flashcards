952d4b9a263d86df869175ad4440f57f9b3d26d9
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-09-11 22:48:14 +0530

    server side errors, best practises, package

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,18 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import db from "./utils/db";
+
+
+const PORT = 5000;
 
 const app = express();
 
-app.get("/", (req, res) => {
+dotenv.config();
+
+app.get("/", (_, res: Response) => {
   res.send("Hello World!");
 });
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+app.listen(PORT, async () => {
+  await db();
 });
